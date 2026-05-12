77753edd3aadc15c5c62f73a7734da00fae4db19
Sahem-test <testroj76@gmail.com>
2026-05-12 12:58:55 +0300

    add remote driver for selenoid

--- src/test/java/guru/qa/classWork/PracticeFormTestsWithJenkins.java
+++ src/test/java/guru/qa/classWork/PracticeFormTestsWithJenkins.java
@@ -14,7 +14,10 @@ public class PracticeFormTestsWithJenkins {
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.browserVersion = "128.0";
         Configuration.pageLoadStrategy = "eager";
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
 
